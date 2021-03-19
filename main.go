847f2c0816a6d98dab311f3a7c0f7a9e64e4e2bc
gergeorg <62167850+gergeorg@users.noreply.github.com>
2021-03-19 05:41:42 +0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+type User struct {
+	Name                  string
+	Age                   uint16
+	Money                 int16
+	Avg_grades, Happiness float64
+	Hobbies               []string
+}
+
+func (u User) getAllInfo() string {
+	return fmt.Sprintf("User name is %s. He is %d and he has money equal: %d", u.Name, u.Age, u.Money)
+}
+
+func home_page(w http.ResponseWriter, r *http.Request) {
+	bob := User{"Bob", 25, -50, 4.2, 0.8, []string{"Football", "Skate", "Dance"}}
+	tmpl, _ := template.ParseFiles("templates/index.html")
+	tmpl.Execute(w, bob)
+}
+
+func contacts_page(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello. This is contacts page")
+}
+
+func handleRequest() {
+	http.HandleFunc("/", home_page)
+	http.HandleFunc("/contacts/", contacts_page)
+	http.ListenAndServe(":8080", nil)
+}
+
+func main() {
+	handleRequest()
+}
